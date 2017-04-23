867a82e89d57efdbeddbd30c5b8feb259bc5d913
Krzysztof Kot <krzykot@gmail.com>
2017-04-23 15:52:39 +0200

    google id

---
+++ src/main/java/kkot/lztimer/service/dto/CurrentStatsDTO.java
@@ -0,0 +1,9 @@
+package kkot.lztimer.service.dto;
+
+/**
+ * Stores current statistics that are displayed in tray icon.
+ *
+ * @author Krzysztof Kot (krzysztof.kot.pl@gmail.com)
+ */
+public class CurrentStatsDTO {
+}
